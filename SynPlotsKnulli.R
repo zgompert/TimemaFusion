f47fb867fed0016000a2c208856a9e0070e5f5e2
Zach Gompert <31893662+zgompert@users.noreply.github.com>
2021-11-08 11:39:56 -0700

    Add files via upload

---
+++ SynPlotsKnulli.R
@@ -0,0 +1,71 @@
+library(data.table)
+
+## read synteny dat
+dat<-fread("out_synteny_knulli.psl",header=FALSE)
+dfdat<-as.data.frame(dat)
+
+tab<-tapply(X=dfdat[,1],INDEX=list(qg=dfdat[,10],tg=dfdat[,14]),sum)
+kn_sc<-as.numeric(unlist(strsplit(x=rownames(tab),split="_"))[seq(2,36,3)])
+tc_sc<-as.numeric(unlist(strsplit(x=colnames(tab),split="_"))[seq(2,50,4)])
+
+## normalize with respect to knulli
+ntab<-tab
+for(i in 1:12){
+	ntab[i,]<-ntab[i,]/sum(ntab[i,])
+}
+ch<-c(7,11,12,13,10,2,9,5,4,3,8,1,6)
+## chrom vs scaf
+cbind(tc_sc,ch)
+#      tc_sc ch
+# [1,] 10660  7
+# [2,] 12033 11
+# [3,] 12380 12
+# [4,] 14101 13
+# [5,] 14160 10
+# [6,] 14640  2
+# [7,] 16151  9
+# [8,] 18722  5
+# [9,] 42912  4
+#[10,] 42935  3
+#[11,]  7748  8
+#[12,]  8483  1
+#[13,]  9928  6
+pdf("SynTcrTknul.pdf",width=6,height=6)
+par(mar=c(5,5,1,1))
+image(ntab,axes=FALSE,xlab="T. knulli",ylab="T. cristinae",cex.lab=1.4)
+axis(2,at=seq(0,13,length.out=13)/13,ch,las=2)
+axis(1,at=seq(0,12,length.out=12)/12,kn_sc,las=2)
+box()
+dev.off()
+
+## get subset Tcr 11 (12033) vs Tknul 500
+tcr11<-grep(x=dfdat[,14],pattern="12033") 
+tkn500<-grep(x=dfdat[,10],pattern="500")
+cc11x500<-tcr11[tcr11 %in% tkn500]
+
+## aprox boudns of SV signal
+lb<-c(12696313,15226220)
+ub<-c(42020457,44744971)
+
+subd<-dfdat[cc11x500,]
+
+## 12/13 qstart qend
+## 16/17 tstart tend
+pdf("AlignTcr11Tknul500.pdf",width=6,height=6)
+par(mar=c(5,5,1,1))
+xub<-54072659
+yub<-65781494
+plot(as.numeric(subd[1,12:13]),as.numeric(subd[1,16:17]),type='l',xlim=c(0,xub),ylim=c(0,yub),cex.lab=1.4,xlab="T. knulli",ylab="T. cristinae")
+
+N<-dim(subd)[1]
+for(i in 2:N){
+	if(subd[i,9]=="++"){
+		lines(subd[i,12:13],subd[i,16:17])
+	}
+	else{
+		lines(subd[i,12:13],subd[i,16:17],col="cadetblue")
+	}
+}
+abline(v=c(lb,ub),col="red",lwd=1.5)
+dev.off()	
+
