d41d8717cf7e6f0a15f5cbc2c321a41d314d2279
Zach Gompert <31893662+zgompert@users.noreply.github.com>
2022-11-28 14:41:46 -0700

    Add files via upload

--- SummarizeDepthBcounts.R
+++ SummarizeDepthBcounts.R
@@ -1,6 +1,6 @@
 library(data.table)
 
-dat<-fread("t_rw_comb_outg_perform_reg_depth.txt",header=FALSE)
+dat<-fread("~/../gompert-group3/data/timema_clines_rw_SV/align_rw_plus/t_rw_knulli_perform_reg_depth.txt",header=FALSE)
 dm<-as.matrix(dat[,-c(1:2)])
 mn<-apply(dm,1,mean)
 prop<-apply(dm > 0,1,mean)
@@ -8,15 +8,23 @@ prop<-apply(dm > 0,1,mean)
 keep<-mn > 2 & prop > 0.8
 
 pos<-as.numeric(unlist(dat[keep,2]))
+length(pos)
+#[1] 160703 = number of sites
 
-save(list=ls(),file="invar_positions.rdat")
 
-## read in file with potentially variable sites, pre-filtering
-## grep ^Sc ~/../gompert-group3/data/timema_clines_rw_SV/variants_rw_plus/t_rw_comb_outg_perform.vcf | cut -f 2 > var_pos.txt 
+save(list=ls(),file="positions.rdat")
 
-pos_invar<-pos[-which(pos %in% vp)]
-length(pos_invar)
-## [1] 60612
-## left 60,612 confident, invariant sites
+## scan all initial called SNPs on scaffold 500
+snps_all<-scan("prefilt_var_pos.txt")
+## scan snps remaining
+snps_kept<-scan("filt_var_pos.txt")
 
-write.table(pos_invar,file="invar_positions_perform_og.txt",row.names=FALSE,col.names=FALSE,quote=FALSE)
+## remove true snps from initial set
+drop<-which(snps_all %in% snps_kept)
+
+## get initially called but then dropped, also includes outside of Perform
+bad_snps <-snps_all[-drop]
+
+pos_keep<-pos[-which(pos %in% bad_snps)]
+## 157212 = number of sites that wouldn't have been filtered out
+
